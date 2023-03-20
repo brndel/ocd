aa6901f327b27f9bce3f229f0a4485adda0e0a62
Armin <ar.braendle@gmail.com>
2023-03-20 22:43:09 +0100

    move formatting of RunnerError in functions

--- src/runner/error.rs
+++ src/runner/error.rs
@@ -1,92 +1,127 @@
-use crate::parser::InteractionTest;
+use std::fmt::Display;
 
+use crate::parser::InteractionTest;
 
 pub enum RunnerError {
     Fail {
-      interaction: InteractionTest,
-      line: usize,
-      expected: String,
-      found: String,
-      prev_output: Vec<String>,
+        interaction: InteractionTest,
+        line: usize,
+        expected: String,
+        found: String,
+        prev_output: Vec<String>,
     },
     Error {
-      interaction: InteractionTest,
-      line: usize,
-      error_message: String,
-      error_code: i32,
-      prev_output: Vec<String>,
+        interaction: InteractionTest,
+        line: usize,
+        error_message: String,
+        error_code: i32,
+        prev_output: Vec<String>,
     },
-  }
-  
-  impl ToString for RunnerError {
+}
+
+fn style<D: Display>(message: D, code: &str) -> String {
+    format!("\x1b[{}m{}\x1b[0m", code, message)
+}
+
+fn style_fail(message: &str) -> String {
+    style(message, "1;91")
+}
+
+fn style_error(message: &str) -> String {
+    style(message, "1;30;41")
+}
+
+fn style_bold<D: Display>(message: D) -> String {
+    style(message, "1")
+}
+
+fn style_green<D: Display>(message: D) -> String {
+    style(message, "32")
+}
+
+fn style_red<D: Display>(message: D) -> String {
+    style(message, "91")
+}
+
+impl ToString for RunnerError {
     fn to_string(&self) -> String {
-      let mut str = String::new();
-  
-      let (mut prev_output, error) = match self {
-        RunnerError::Fail {
-          interaction,
-          line,
-          expected,
-          found,
-          prev_output,
-        } => {
-          str += format!(
-            "\x1b[1;91mFailed\x1b[0m {}:{}\n",
-            interaction.file_path, line
-          )
-          .as_str();
-          str += format!(
-            "\x1b[1m{}\x1b[0m in line \x1b[1m{}\x1b[0m\n",
-            interaction.name, line
-          )
-          .as_str();
-          str += format!("expected: '{}'\n", expected).as_str();
-          str += format!("found:    '{}'\n", found).as_str();
-  
-          (prev_output.to_owned(), found)
+        let mut str = String::new();
+
+        let (mut prev_output, error) = match self {
+            RunnerError::Fail {
+                interaction,
+                line,
+                expected,
+                found,
+                prev_output,
+            } => {
+                str += format!(
+                    "{} {}:{}\n",
+                    style_fail("Failed"),
+                    interaction.file_path,
+                    line
+                )
+                .as_str();
+                str += format!(
+                    "{} in line {}\n",
+                    style_bold(&interaction.name),
+                    style_bold(line)
+                )
+                .as_str();
+                str += format!("expected: '{}'\n", expected).as_str();
+                str += format!("found:    '{}'\n", found).as_str();
+
+                (prev_output.to_owned(), found)
+            }
+            RunnerError::Error {
+                interaction,
+                line,
+                error_message,
+                error_code,
+                prev_output,
+            } => {
+                str += format!(
+                    "{} in {}:{}\n",
+                    style_error("Error"),
+                    interaction.file_path,
+                    line
+                )
+                .as_str();
+                str += format!(
+                    "{} in line {} with error code {}\n",
+                    style_bold(&interaction.name),
+                    style_bold(line),
+                    style_bold(error_code)
+                )
+                .as_str();
+                str += format!("{}\n", error_message).as_str();
+
+                (prev_output.to_owned(), error_message)
+            }
+        };
+
+        str += style_bold("Previous output:\n").as_str();
+
+        let mut prev_output_str = String::new();
+
+        if prev_output.len() > 10 {
+            prev_output_str += "[...]\n"
+        } else {
+            prev_output_str += "<start>\n"
         }
-        RunnerError::Error {
-          interaction,
-          line,
-          error_message,
-          error_code,
-          prev_output,
-        } => {
-          str += format!(
-            "\x1b[1;30m\x1b[41mError\x1b[0m in {}:{}\n",
-            interaction.file_path, line
-          )
-          .as_str();
-          str += format!(
-            "\x1b[1m{}\x1b[0m in line \x1b[1m{}\x1b[0m with error code \x1b[1m{}\x1b[0m\n",
-            interaction.name, line, error_code
-          )
-          .as_str();
-          str += format!("{}\n", error_message).as_str();
-  
-          (prev_output.to_owned(), error_message)
-        }
-      };
-  
-      str += "\x1b[1mPrevious ouput:\x1b[0m\n";
-  
-      str += "\x1b[32m";
-      if prev_output.len() > 10 {
-        str += "[...]\n"
-      } else {
-        str += "<start>\n"
-      }
-  
-      prev_output.reverse();
-      prev_output.truncate(10);
-      prev_output.reverse();
-  
-      str += prev_output.join("\n").as_str();
-  
-      str += "\x1b[0m\n";
-  
-      str += format!("\x1b[91m{}\x1b[0m", error).as_str();
-  
-      str
+
+        prev_output.reverse();
+        prev_output.truncate(10);
+        prev_output.reverse();
+
+        prev_output_str += prev_output.join("\n").as_str();
+
+        prev_output_str += "\n";
+
+        str += style_green(prev_output_str).as_str();
+
+        str += style_red(error).as_str();
+
+        str
     }
-  }
+}
